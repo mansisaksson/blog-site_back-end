149af2cbff552598df082a72d04c732046ce7ce8
Måns Isaksson <mans.95.isaksson@gmail.com>
2018-07-05 15:31:59 +0200

    express sessions

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
-import { RequestServer } from './request_server/RequestServer'
+import * as express from "express"
+import * as session from 'express-session'
+var app = express()
 
-__dirname;
-RequestServer.start(3000, __dirname + '/auth_server/requests');
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'rFeshvHyphDvunfzKQubevCWRHBA4r3XXzJFA677EwZfkSTfPUB9D3eAb6cr2VDTcfVDERzHQagchwxYtnZ6n5NnDQP77HRjebMS',
+  resave: false,
+  saveUninitialized: true,
+	cookie: { secure: true }
+}))
+
+app.get('/', function(req: express.Request, res: express.Response, next) {
+	req.session['test'] = 100
+
+	next()
+})
+
+app.all('/', function(req, res) {
+
+})
+
+app.listen(3000,  function () {
+	console.log("Server started!")
+})
