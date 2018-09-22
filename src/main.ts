4816add406e9842017dd1604977cd0ca9ff65383
mansisaksson <mans.95.isaksson@gmail.com>
2018-09-22 21:24:20 +0200

    todo

--- src/main.ts
+++ src/main.ts
@@ -61,3 +61,28 @@ mongoose.connect(uri).then((Mongoose) => {
 		mongoose.disconnect()
 	})
 }).catch(e => { throw e })
+
+// TODO:
+// process.stdin.resume();//so the program will not close instantly
+
+// function exitHandler(options, exitCode) {
+// 	console.log("******************************************** CLOSE ********************************************")
+// 	mongoose.disconnect().then(() => {
+// 		process.exit()
+// 	}).catch(() => {
+// 		process.exit()
+// 	})
+// }
+
+// //do something when app is closing
+// process.on('exit', exitHandler.bind(null, { cleanup: true }));
+
+// //catches ctrl+c event
+// process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+
+// // catches "kill pid" (for example: nodemon restart)
+// process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+// process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+
+// //catches uncaught exceptions
+// process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
